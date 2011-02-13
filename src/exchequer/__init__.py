317c84c9ace42ecef314d11674aecbb7e946cc22
Zachary Voase <z@zacharyvoase.com>
2011-02-13 16:02:13 +0000

    Refactored column lengths into a separate function.

--- src/exchequer/__init__.py
+++ src/exchequer/__init__.py
@@ -1,6 +1,37 @@
 import itertools
 
 
+def column_lengths(rows):
+    """
+    Get the maximum column lengths of a given iterable of rows.
+
+    Returns an empty tuple if no rows are given. All rows must be of the same
+    length, otherwise a :exc:`ValueError` will be raised. This function is
+    unicode-ready; :func:`unicode` will be called on every item to get its
+    string representation (for determining length).
+
+        >>> column_lengths([('a', 'bcdef', 'gh'), ('fed', 'up', 'boring')])
+        (3, 5, 6)
+        >>> column_lengths([])
+        ()
+        >>> column_lengths([('a', 'b'), ('1', '2', '3')])
+        Traceback (most recent call last):
+        ...
+        ValueError: Row 1: expected 2 columns, got 3
+    """
+    columns = []
+    for i, row in enumerate(rows):
+        if columns and len(columns) != len(row):
+            raise ValueError("Row %d: expected %d columns, got %d" % (
+                i, len(columns), len(row)))
+        elif not columns:
+            columns = [len(unicode(cell)) for cell in row]
+        else:
+            for j, cell in enumerate(row):
+                columns[j] = max(len(unicode(cell)), columns[j])
+    return tuple(columns)
+
+
 def print_table(rows, header=True):
     """
     Print a list of rows as a text table.
@@ -19,18 +50,7 @@ def print_table(rows, header=True):
         Harriet | 2202  | Sales
     """
     first_pass, second_pass = itertools.tee(iter(rows))
-    try:
-        columns = [len(cell) for cell in first_pass.next()]
-        len_columns = len(columns)
-    except StopIteration:
-        raise ValueError("Can't print an empty table")
-
-    for i, row in enumerate(first_pass):
-        if len(row) != len_columns:
-            raise ValueError("Row %d: expected %d columns, got %d" % (
-                i + 1, len_columns, len(row)))
-        for j, cell in enumerate(row):
-            columns[j] = max(columns[j], len(str(cell)))
+    columns = column_lengths(first_pass)
 
     format_row = lambda row: ' | '.join(str(cell).ljust(length) for cell, length in zip(row, columns))
     print format_row(second_pass.next()).rstrip()
